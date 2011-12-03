e6cf570fd9cea48a1686d5517d5d6499d3e2322e
Bart Lantz <bartlantz@gmail.com>
2011-12-03 16:00:30 -0700

    filled in getters and setters to Request Object

--- Request.java
+++ Request.java
@@ -8,9 +8,21 @@ public class Request {
     private boolean readRequest;
     private int blockNumber;
     private byte[] data;
-    public Request(int blockNumber, byte data[]) {
+    public Request(int blockNumber, byte data[], boolean readRequest) {
         this.blockNumber = blockNumber;
-        
+        this.data        = data;
+        this.readRequest = readRequest;
+    }
+    public int getBlocks() {
+        return blockNumber;
+    }
+    
+    public byte[] getData() {
+        return data;
+    }
+    
+    public boolean getReadRequest() {
+        return readRequest;
     }
 
 }
