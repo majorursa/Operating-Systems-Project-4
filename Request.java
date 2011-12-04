1d5d219b6fb2cf35295ab9f8eae3c766c5d8afee
Bart Lantz <bartlantz@gmail.com>
2011-12-03 20:37:25 -0700

    added finished flag with getter/setters to Request class

--- Request.java
+++ Request.java
@@ -8,10 +8,12 @@ public class Request {
     private boolean readRequest;
     private int blockNumber;
     private byte[] data;
+    private boolean finished;
     public Request(int blockNumber, byte data[], boolean readRequest) {
         this.blockNumber = blockNumber;
         this.data        = data;
         this.readRequest = readRequest;
+        this.finished = false;
     }
     public int getBlocks() {
         return blockNumber;
@@ -21,8 +23,25 @@ public class Request {
         return data;
     }
     
+    /** Returns if request is a read request
+     *
+     * @return a <code>boolean</code> value
+     */
     public boolean getReadRequest() {
         return readRequest;
+    }
+    
+    /** Sets finished flag so process can return
+     */
+    public void setFinished() {
+        finished = true;
+    }
+    
+    /** Returns whether or not IO call is finished.
+     * @return finished flag
+     */
+    public boolean getFinished() {
+        return finished;
     }
 
 }
